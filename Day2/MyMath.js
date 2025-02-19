5ac3affa9e309e02fefc1ab693a03795d92b09db
nikhil pandit <nikhilpandit264@gmail.com>
2025-02-19 16:39:40 +0530

    Show Index.html to server side

---
+++ Day2/MyMath.js
@@ -0,0 +1,11 @@
+let sum =(a,b) =>{return a+b};
+let sub =(a,b) =>{return a-b};
+let div =(a,b) =>{return a/b};
+// module.exports={sum,sub,div};
+
+const obj={
+    sum,
+    sub,
+    div
+}
+module.exports=obj;
---
+++ Day2/p1.js
@@ -0,0 +1,7 @@
+// const {sum, div} =require("./MyMath")
+// console.log(sum(2,3));
+// console.log(div(4,2));
+
+const obj =require("./MyMath")
+console.log(obj.sum(2,3))
+console.log(obj.div(4,2));
